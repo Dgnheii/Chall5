ea6c82555f3ef6c369b1a6ad589eb792f64a31d5
Dgnheii <102909809+Dgnheii@users.noreply.github.com>
2022-10-25 10:18:33 +0700

    Add files via upload

---
+++ chall5/libs/students.php
@@ -0,0 +1,129 @@
+<?php
+global $conn;
+
+function connect_db()
+{
+    global $conn;
+    if (!$conn){
+        $conn = mysqli_connect('localhost', 'root', 'vertrigo', 'qlsv') or die ('Can\'t not connect to database');
+        mysqli_set_charset($conn, 'utf8');
+    }
+}
+ 
+function disconnect_db()
+{
+    global $conn;
+    if ($conn){
+        mysqli_close($conn);
+    }
+}
+
+function get_all_students()
+{
+    global $conn;
+    connect_db();
+    $sql = "select * from sinhvien where role = 0 order by username";
+    $query = mysqli_query($conn, $sql);
+    $result = array();
+    if ($query){
+        while ($row = mysqli_fetch_assoc($query)){
+            $result[] = $row;
+        }
+    }
+    return $result;
+}
+ 
+function get_student($username)
+{
+    global $conn;
+    connect_db();
+    $sql = "select * from sinhvien where username = '$username' and role = 0 ";
+    $query = mysqli_query($conn, $sql);
+    $result = mysqli_fetch_assoc($query);
+    return $result;
+}
+
+function get_teacher($username)
+{
+    global $conn;
+    connect_db();
+
+    $username = mysqli_real_escape_string($conn, htmlentities($username));
+    $sql = "select * from sinhvien where username = '$username' and role = 1 ";
+    
+    $query = mysqli_query($conn, $sql);
+    $result = mysqli_fetch_assoc($query);
+    return $result;
+}
+ 
+function add_student($username,$password, $name, $email, $phone)
+{
+    global $conn;
+    connect_db();
+    
+    $username = mysqli_real_escape_string($conn, htmlentities($username));
+    $password = mysqli_real_escape_string($conn, htmlentities($password));
+    $name = mysqli_real_escape_string($conn, htmlentities($name));
+    $email = mysqli_real_escape_string($conn, htmlentities($email));
+    $phone = mysqli_real_escape_string($conn, htmlentities($phone));
+
+    $sql = "
+            INSERT INTO sinhvien(username,password, name, email, phone, role) VALUES
+            ('$username','$password', '$name', '$email', '$phone', '0')
+    ";
+    $query = mysqli_query($conn, $sql);
+    return $query;
+}
+ 
+function edit_student($currentName, $username,$password, $name, $email, $phone)
+{
+    global $conn;
+    connect_db();
+
+    $currentName = mysqli_real_escape_string($conn, htmlentities($currentName));
+    $username = mysqli_real_escape_string($conn, htmlentities($username));
+    $password = mysqli_real_escape_string($conn, htmlentities($password));
+    $name = mysqli_real_escape_string($conn, htmlentities($name));
+    $email = mysqli_real_escape_string($conn, htmlentities($email));
+    $phone = mysqli_real_escape_string($conn, htmlentities($phone));
+
+    $sql = "
+            UPDATE sinhvien SET
+            username = '$username',
+            password = '$password',
+            name = '$name',
+            email = '$email',
+            phone = '$phone'
+            WHERE username = '$currentName'
+    ";
+    $query = mysqli_query($conn, $sql);
+    return $query;
+}
+ 
+function delete_student($username)
+{
+    global $conn;
+    connect_db();
+    $username = mysqli_real_escape_string($conn, htmlentities($username));
+    $sql = "
+            DELETE FROM sinhvien
+            WHERE username = '$username'
+    ";
+    $query = mysqli_query($conn, $sql);
+     
+    return $query;
+}
+
+function checkUsername($username){
+    global $conn;
+    connect_db();
+    $username = mysqli_real_escape_string($conn, htmlentities($username));
+    $sql = "select * from sinhvien where role = 0 and username = '$username' ";
+    $query = mysqli_query($conn, $sql);
+    $result = mysqli_fetch_assoc($query);
+    if(!empty($result)){
+        return TRUE;
+    } else {return FALSE;}
+}
+
+?>
---
+++ chall5/student-edit.php
@@ -0,0 +1,167 @@
+<?php
+ 
+require './libs/students.php';
+session_start();
+$username = isset($_GET['username']) ? $_GET['username'] : '';
+
+if ($username){
+    if($_SESSION['role'] == 1 && $username == $_SESSION['username']){
+        $data = get_teacher($username);
+    } elseif($_SESSION['role'] == 0 && $username == $_SESSION['username']) {
+        $data = get_student($username);
+    } elseif($_SESSION['role'] == 1 && $username != $_SESSION['username']) {
+        $data = get_student($username);
+    }
+    $_SESSION['currentName'] = $data['username'];
+    $_SESSION['userRole'] = $data['role'];
+}
+if (!$data){
+   header("location: student-list.php");
+}
+if (!empty($_POST['edit_student']))
+{
+    
+    $data['username'] = isset($_POST['username']) ? $_POST['username'] : '';
+    $data['password'] = isset($_POST['password']) ? $_POST['password'] : '';
+    $data['name'] = isset($_POST['name']) ? $_POST['name'] : '';
+    $data['email'] = isset($_POST['email']) ? $_POST['email'] : '';
+    $data['phone'] = isset($_POST['phone']) ? $_POST['phone'] : '';
+    $errors = array();
+    if (empty($data['username'])){
+        $errors['username'] = 'Chưa nhập tên đăng nhập sinh viên';
+    }
+    if(checkUsername($data['username']) == TRUE) {
+        $errors['username'] = 'Tên đăng nhập đã tồn tại';
+    }
+
+    if (empty($data['password'])){
+        $errors['password'] = 'Chưa nhập mật khẩu sinh viên';
+    }
+
+    if (empty($data['name'])){
+        $errors['name'] = 'Chưa nhập tên sinh viên';
+    }
+     
+    if (empty($data['email'])){
+        $errors['email'] = 'Chưa nhập email sinh viên';
+    }
+
+    if (empty($data['phone'])){
+        $errors['phone'] = 'Chưa nhập số điện thoại sinh viên';
+    }
+     
+    if (!$errors){
+        edit_student($_SESSION['currentName'], $data['username'], $data['password'], $data['name'], $data['email'], $data['phone']);
+        if($_SESSION['username'] == $_SESSION['currentName']){
+            $_SESSION['username'] = $data['username']; 
+        }
+        if($_SESSION['userRole'] == 1){
+            header("location: home.php");
+        } else {
+            header("location: student-info.php?username=".$data['username']);
+        }    
+    }
+}
+ 
+disconnect_db();
+?>
+ 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sửa thông tin</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style type="text/css">
+            table {
+            margin: 8px;
+            }
+
+            body {
+                background: #bff;
+            }
+            
+            input {
+                background: #bff;
+            }
+
+            th {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: .8em;
+            background: #b8e1ff;
+            color: #fff;
+            padding: 2px 6px;
+            border-collapse: separate;
+            border: 1px solid #000;
+            }
+            
+            td {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: .9em;
+            border: 1px solid #000;
+            background: #bff;
+            }
+            input {
+                background: #b8e1ff;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Sửa thông tin</h1>
+        <?php if($_SESSION['username'] == $_SESSION['currentName']) { ?>
+            <a href="home.php">Home</a></br> </br>
+        <?php } else { ?>
+            <a href="student-list.php">Danh sách sinh viên</a> </br> </br>
+        <?php } ?>
+        <form method="post" action="student-edit.php">
+        <input type="hidden" name="id" value="<?php echo $_SESSION['userID']; ?>"/>
+            <table width="50%" border="1" cellspacing="0" cellpadding="10">
+                <tr>
+                    <?php if($_SESSION['role'] == 1) { ?>
+                        <td>Username</td>
+                        <td>
+                            <input type="text" name="username" value="<?php echo !empty($data['username']) ? $data['username'] : ''; ?>"/>
+                            <?php if (!empty($errors['username'])) echo $errors['username']; ?>
+                        </td>
+                    <?php } ?>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td>
+                        <input type="text" name="password" value='<?php echo !empty($data['password']) ? $data['password'] : ''; ?>'/>
+                        <?php if (!empty($errors['password'])) echo $errors['password']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <?php if($_SESSION['role'] == 1) { ?>
+                        <td>Name</td>
+                        <td>
+                            <input type="text" name="name" value="<?php echo !empty($data['name']) ? $data['name'] : ''; ?>"/>
+                            <?php if (!empty($errors['name'])) echo $errors['name']; ?>
+                        </td>
+                    <?php } ?>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td>
+                        <input type="text" name="email" value="<?php echo !empty($data['email']) ? $data['email'] : ''; ?>"/>
+                        <?php if (!empty($errors['email'])) echo $errors['email']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Phone</td>
+                    <td>
+                        <input type="text" name="phone" value="<?php echo !empty($data['phone']) ? $data['phone'] : ''; ?>"/>
+                        <?php if (!empty($errors['phone'])) echo $errors['phone']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type="submit" name="edit_student" value="Lưu"/>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
